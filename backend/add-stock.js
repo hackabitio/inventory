cb50a0727f33c9bcdfd7792f5eb662997811e958
foadyousefi <foadyousefi@gmail.com>
2022-08-28 19:12:00 +0200

    Function for adding stock to inventory

--- backend/add-stock.js
+++ backend/add-stock.js
@@ -2,9 +2,10 @@ let jsoning = require("jsoning")
 const generateQr = require('./qrcode')
 const uuid = require('uuid')
 
-let db = new jsoning("./db/stock.json")
+let stockDb = new jsoning("./db/stock.json")
+let addingDb = new jsoning("./db/adding.json")
 
-module.exports = (ctx) => {
+const addProduct = (ctx) => {
   let product = ctx.request.body.product
   product.id = uuid.v4()
   let sku = product.sku
@@ -14,8 +15,7 @@ module.exports = (ctx) => {
   } else {
     sku = product.name.toLowerCase().replace(/\s/g, '-')
   }
-
-  db.push(sku, product)
+  stockDb.push(sku, product)
 
   ctx.body = {
     msg: 'Products added',
@@ -23,3 +23,42 @@ module.exports = (ctx) => {
   }
   ctx.status = 201
 }
+
+const addStock = (ctx) => {
+  let product = ctx.request.body.product
+  let id = uuid.v4()
+  let sku = product.sku
+  if (typeof sku === 'undefined') {
+    ctx.body = 'product SKU is required'
+    ctx.status = 201
+  } else {
+    sku = sku.toLowerCase().replace(/\s/g, '-')
+    // Get the product from available stock database stockDb
+    let theProduct = stockDb.get(sku)
+    if (theProduct) {
+      if (Array.isArray(theProduct)) {
+        theProduct = theProduct[0]
+      }
+      addingDb.push(id, {
+        id,
+        time: new Date(),
+        sku: theProduct.sku,
+        qty: product.qty
+      })
+      let oldQty = theProduct.qty
+      let newQty = oldQty + product.qty
+      theProduct.qty = newQty
+      stockDb.set(sku, theProduct)
+
+      ctx.body = {
+        msg: 'Products updated!',
+        newQty,
+        id: theProduct.id
+      }
+      ctx.status = 201
+    }
+
+  }
+}
+
+module.exports = { addProduct, addStock }
--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,16 @@ const parser = require("koa-bodyparser")
 const cors = require("@koa/cors")
 const Router = require("koa-router")
 const generateQr = require('./qrcode')
-const addStock = require('./add-stock')
+const addProduct = require('./add-stock').addProduct
+const addStock = require('./add-stock').addStock
 
 const router = new Router()
 const App = new Koa()
 const port = 8000
 
-
-
 router.post("/qr", generateQr)
+router.post("/add-product", addProduct)
 router.post("/add-stock", addStock)
-
-
 
 App.use(parser())
   .use(cors())
