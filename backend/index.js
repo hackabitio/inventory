f17c688b88ced957cfc6457df067054b74a5a142
foadyousefi <foadyousefi@gmail.com>
2022-08-29 00:04:10 +0200

    Rename some backend files

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,12 @@ const parser = require("koa-bodyparser")
 const cors = require("@koa/cors")
 const Router = require("koa-router")
 const generateQr = require('./qrcode')
-const addProduct = require('./add-stock').addProduct
-const addStock = require('./add-stock').addStock
-const releaseStock = require('./add-stock').releaseStock
-const getProducts = require('./add-stock').getProducts
-const getProduct = require('./add-stock').getProduct
+const addProduct = require('./stock').addProduct
+const addStock = require('./stock').addStock
+const releaseStock = require('./stock').releaseStock
+const getProducts = require('./stock').getProducts
+const getProduct = require('./stock').getProduct
+const getAddStock = require('./stock').getAddStock
 
 const router = new Router()
 const App = new Koa()
@@ -19,6 +20,7 @@ router.post("/add-stock", addStock)
 router.post("/release-stock", releaseStock)
 router.get("/products", getProducts)
 router.get("/product", getProduct)
+router.get("/get-addings", getAddStock)
 
 App.use(parser())
   .use(cors())
