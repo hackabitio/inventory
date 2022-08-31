e85cc9c9de6623602bd9ee97ac0cf1e09d00f59d
foadyousefi <foadyousefi@gmail.com>
2022-08-31 18:08:34 +0200

    Add sass support

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
-
+import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -9,7 +9,8 @@ const config = {
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
 		}
-	}
+	},
+	preprocess: preprocess()
 };
 
 export default config;
