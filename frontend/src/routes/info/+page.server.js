794cd76ebf0a4037b0f1e43312c9c227ec50fd25
foadyousefi <foadyousefi@gmail.com>
2022-09-16 15:22:41 +0200

    Get info from svelete

--- frontend/src/routes/info/+page.server.js
+++ frontend/src/routes/info/+page.server.js
@@ -1,23 +1,9 @@
-import {error} from "@sveltejs/kit"
-import { api } from './api'
+import { getProducts, getCategories } from '$lib/data'
 
 export const load = async () => {
-  const response = await api('GET', 'products?details=true')
-  const categories = await api('GET', 'categories')
 
-  if (response.status === 404 && categories.status === 404) {
-    return {
-      stock: [],
-      categories: []
-    }
+  return {
+    products: await getProducts(true),
+    categories: await getCategories()
   }
-
-  if (response.status === 200 && categories.status === 200) {
-    return {
-      products: await response.json(),
-      categories: await categories.json()
-    }
-  }
-
-  throw error(response.status)
 }
